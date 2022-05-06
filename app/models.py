6b1efd7d1f3d14d83db36d2aa98d5c592ccf9196
Christine Mwangi <christine.mwangi@student.moringaschool.com>
2022-05-06 20:27:51 +0300

    ft:created two classes for defining the objects

---
+++ app/models.py
@@ -0,0 +1,21 @@
+class Articles:
+    def __init__(self,image,decription,url,title,author):
+        self.image = image
+        self.decription =ription
+        self.url = url
+        self.title = title
+        self.author = author
+
+class Sources:
+    '''
+    defines source objects
+    '''
+    
+    def _init_(self,id,name,description,url,category,country,language):
+        self.id = id
+        self.name = name
+        self.description = description
+        self.url = url
+        self.category = category
+        self.country = country
+        self.language = language
