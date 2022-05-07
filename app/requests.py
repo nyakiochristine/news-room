055ecc61b677f5674160c29a2913276a75b8ffc9
Christine Mwangi <christine.mwangi@student.moringaschool.com>
2022-05-07 10:40:58 +0300

    ft:added a fuction to get news sources api

--- app/requests.py
+++ app/requests.py
@@ -4,7 +4,7 @@ from .models import Articles,Sources
 
 apikey =None
 
-BASE_SOURCE_URL = None
+BASE_SOURCES_URL = None
 BASE_ARTICLES_URL = None
 BASE_CATEGORY_URL = None
 BASE_SEARCH_URL = None
@@ -13,10 +13,32 @@ BASE_SEARCH_URL = None
 def configure_request(app):
     global api_key,BASE_SOURCE_URL,BASE_ARTICLES_URL,BASE_CATEGORY_,BASE_SEARCH_URL
     api_key = app.config('API_KEY')
-    BASE_SOURCE_URL = app.config['SOURCE_URL']
+    BASE_SOURCES_URL = app.config['SOURCES_URL']
     BASE_ARTICLES_URL = app.config['ARTICLE_URL']
     BASE_CATEGORY_URL = app.config['CATEGORY_URL']
     BASE_SEARCH_URL= app.config ['SEARCH_URL']
+def get_sources():
+    news_source_url = BASE_SOURCES_URL.format(api_key)
+    with urllib.request.urlopen(news_source_url) as news_source:
+        source_info = news_source.read()
+        source_info_dict = json.loads(source_info)
+        sources_results = None
+        
+        if source_info_dict['sources']:
+            sources_list= source_info_dict['sources']
+            sources_results = process_sources(sources_list)
+        return sources_results
+    
+    
+def process_sources(sources):
+    
+        
+        
+    
+    
+    
+
+    
     
     
 def search_news(word):
@@ -28,7 +50,7 @@ def search_news(word):
         
         search_news_results= None
         
-        if search_news_info_dict['articles']
+        if search_news_info_dict['articles']:
             search_news_list = search_news_info_dict['articles']
             search_news_results = (search_news_list)
 
