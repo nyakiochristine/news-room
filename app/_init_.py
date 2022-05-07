1ec16f34c09bc715cf8d3e61e7249ade8101b795
Christine Mwangi <christine.mwangi@student.moringaschool.com>
2022-05-07 17:29:41 +0300

    ft:initialised bootstrap  and set up configuration

--- app/_init_.py
+++ app/_init_.py
@@ -1,5 +1,32 @@
 from flask import Flask
 from flask_bootstrap import Bootstrap
 
-
 bootstrap = Bootstrap()
+
+def create_app(config_name):
+    app=Flask(__name__)
+    app.config.from_object([config_name])
+    
+    bootstrap.init_app(app)
+
+    from .main import main as Blueprint
+    app.register_blueprint(Blueprint)
+    
+#setting up configuration
+    from .requests import configure_request
+    configure_request(app)
+    
+    return app
+    
+
+
+
+
+
+
+
+
+
+
+
+
