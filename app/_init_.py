561e854eecf459caa4840f7a84e2356e98b2aee6
Christine Mwangi <christine.mwangi@student.moringaschool.com>
2022-05-09 12:59:22 +0300

    ft:imported config options

--- app/_init_.py
+++ app/_init_.py
@@ -1,17 +1,24 @@
 from flask import Flask
 from flask_bootstrap import Bootstrap
+from config import config_options
 
 bootstrap = Bootstrap()
 
 def create_app(config_name):
     app=Flask(__name__)
-    app.config.from_object([config_name])
+    app.config.from_object(config_options[config_name])
     
     bootstrap.init_app(app)
+
+
+
 
     from .main import main as Blueprint
     app.register_blueprint(Blueprint)
     
+
+
+
 #setting up configuration
     from .requests import configure_request
     configure_request(app)
