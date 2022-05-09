c8968b56f3eca9067919c0f480a0db2894be2bf7
Christine Mwangi <christine.mwangi@student.moringaschool.com>
2022-05-09 13:00:14 +0300

    ft:added routing

--- app/main/views.py
+++ app/main/views.py
@@ -2,8 +2,8 @@
 
 from flask import render_template, request,redirect, url_for
 import flask
-from app import main
-from ..requests import get_sources,get_news_articles, search_news
+from . import main
+from ..requests import get_sources,get_articles, get_articles_category,search_news
 
 @main.route('/')
 def index():
@@ -14,7 +14,31 @@ def index():
         return redirect(url_for('.search',word=search_news))
     else :return render_template('index.html',title=title,sources=sources)
     
-    
+@main.route('/news/<id>')
+def articles(id):
+    title = "Article-News"
+    articles = get_articles(id)
+    return render_template('articles.html', title=title, articles=articles)
+
+@main.route('/sports/')
+def sports():
+    title = "Sports Highlights"
+    articles = get_articles_category('sports')
+    return render_template('sports.html', title=title, articles=articles)
+
+@main.route('/business/')
+def business():
+    title = "Business Highlights"
+    articles = get_articles_category('business')
+    return render_template('business.html', title=title, articles=articles)
+
+@main.route('/technology')
+def technology():
+    title = "Technology Highlights"
+    articles = get_articles_category('technology')
+    return render_template('technology.html', title=title, articles=articles)
+
+     
 
 
 
@@ -28,3 +52,4 @@ def search(word):
     searched_word = word_format.split
     title = flask; 'Results for {word}'
     return render_template('search.html',news=searched_word,title=title)
+
