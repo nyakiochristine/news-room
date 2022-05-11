0431ae8e83ce16c1d8d22343bbcba9c2bc7fce8c
Christine Mwangi <christine.mwangi@student.moringaschool.com>
2022-05-11 12:02:05 +0300

    ft:patch:fixed a bug

--- app/main/views.py
+++ app/main/views.py
@@ -5,15 +5,19 @@ import flask
 from . import main
 from ..requests import get_sources,get_articles, get_articles_category
 
+#@main.route('/')
+#def index():
+    #title = 'NewsRoom'
+    #sources = get_sources
+    #search_news =request.args.get('news-query')
+   # if search_news :
+        #return redirect(url_for('.search',word=search_news))
+    #return render_template('index.html',title=title,sources=sources)
 @main.route('/')
 def index():
-    title = 'NewsRoom'
-    sources = get_sources
-    search_news =request.args.get('news-query')
-    if search_news :
-        return redirect(url_for('.search',word=search_news))
-    else :return render_template('index.html',title=title,sources=sources)
+    return render_template('index.html')
     
+       
 @main.route('/news/<id>')
 def articles(id):
     title = "Article-News"
--- config.py
+++ config.py
@@ -8,13 +8,18 @@ class Config:
     SEARCH_URL = 'https://newsapi.org/v2/everything?q={}&apiKey={}'
     API_KEY = os.environ.get('API_KEY')
     
+    @staticmethod
+    def init_app(app):
+        pass
+    
     
 class ProdConfig:
-    SOURCES_URL = 'http://newsapi.org/v2/top-headlines/sources?apiKey={}'
-    ARTICLE_URL = 'https://newsapi.org/v2/top-headlines?sources={}&apiKey={}'
-    CATEGORY_URL = 'https://newsapi.org/v2/'
-    SEARCH_URL = 'https://newsapi.org/v2/everything?q={}&apiKey={}'
-    API_KEY = os.environ.get('API_KEY')
+    
+    
+    pass
+    
+     
+    
     
     
 class DevConfig(Config):
